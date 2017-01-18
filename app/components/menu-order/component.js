d4ea6822b68aec160863990f7c8a979635f47851
sujkid <suja.sreekumar@yahoo.com>
2017-01-17 21:54:56 -0500

    Mofify landing page and daily menu
    Modified landing page to get rid of ember-side-menu for now..
    Updated code for daily menu to get the dates and set the ids from
    back-end rather than hardcoding the values in the front-end.

--- app/components/menu-order/component.js
+++ app/components/menu-order/component.js
@@ -10,6 +10,9 @@ export default Ember.Component.extend({
       console.log('order create', this.get('newOrder'));
       let data = this.get('newOrder');
       // console.log('data is ', data);
+      if(!this.get('newOrder.destaddress') || !this.get('newOrder.quantity')) {
+        return;
+      }
       data.menu = this.get('menu'); // identifying which menu the order belongs to
       this.sendAction('createOrder', data);
     }
