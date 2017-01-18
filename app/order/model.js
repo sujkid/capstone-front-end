097611f4fabd8dd4add7bf9bf78830770f7e9b4c
sujkid <suja.sreekumar@yahoo.com>
2017-01-17 23:41:56 -0500

    Render My Orders
    My Order page now renders current users' orders.
    Order model serializer changed to reflect info coming from back end

--- app/order/model.js
+++ app/order/model.js
@@ -3,5 +3,8 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   destaddress: DS.attr('string'),
   quantity: DS.attr('number'),
+  foodname: DS.attr('string'),
+  fooddescription: DS.attr('string'),
+  date: DS.attr('date'),
   menu: DS.belongsTo('menu'),
 });
