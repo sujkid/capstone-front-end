4a56cf88ac7c38489bcb514f89c3282bce348b03
sujkid <suja.sreekumar@yahoo.com>
2017-01-19 16:30:42 -0500

    Add flash messages for order. Update like button
    Order button now has a flash message indicating if order was
    created, and a warning if fields are incomplete.
    Like button is just a regular button with a like text on it.
    For some crazy reason, deployed site is not rendering images from
    local images even though it does in localhost! So got rid of that!!

--- app/components/menu-order/component.js
+++ app/components/menu-order/component.js
@@ -1,16 +1,20 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  flashMessages: Ember.inject.service(),
+
   newOrder: {
     destaddress: null,
     quantity: null,
   },
+
   actions: {
     createOrder () {
       // console.log('order create', this.get('newOrder'));
       let data = this.get('newOrder');
       // console.log('data is ', data);
       if(!this.get('newOrder.destaddress') || !this.get('newOrder.quantity')) {
+        this.get('flashMessages').warning('Oops! Looks like a field is missing!');
         return;
       }
       data.menu = this.get('menu'); // identifying which menu the order belongs to
--- app/menus/orders/route.js
+++ app/menus/orders/route.js
@@ -1,6 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  flashMessages: Ember.inject.service(),
+
   model (params) {
     return this.get('store').findRecord('menu', params.menu_id);
   },
@@ -9,7 +11,8 @@ export default Ember.Route.extend({
       // console.log('hey, i am going to create an order', newOrder);
       let order = this.get('store').createRecord('order', newOrder);
       // console.log("inside the route create order, newOrder is " , order);
-      order.save();
+      order.save()
+        .then(() => this.get('flashMessages').success('Check your cart for your order!'));
     },
     voteUp (menu) {
       // console.log('am i here??', menu.data.votes);
