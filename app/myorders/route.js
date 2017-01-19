56c49e9e81264c29658b080255efe2e4757a8e21
sujkid <suja.sreekumar@yahoo.com>
2017-01-18 23:26:10 -0500

    Delete action to cancel a request
    An order can now be deleted. Routes, components created to do so

--- app/myorders/route.js
+++ app/myorders/route.js
@@ -4,5 +4,11 @@ export default Ember.Route.extend({
   model () {
     let data = this.get('store').findAll('order');
     return data;
-  }
+  },
+  actions : {
+    cancelOrder (myorder) {
+      console.log('myorder');
+      myorder.destroyRecord();
+    },
+  },
 });
